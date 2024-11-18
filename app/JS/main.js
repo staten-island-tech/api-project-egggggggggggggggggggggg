5af2a94894309970bf6566e9dd524c37ad8e778d
johnnyw <jwu38438@gmail.com>
2024-11-18 14:01:22 -0500

    egege

---
+++ app/JS/main.js
@@ -0,0 +1,9 @@
+import "../css/style.css";
+const player_name =  'Junenaah'
+const api_link = `https://api.hypixel.net/player?key=${temporary_key}&name=${player_name}`;
+console.log(api_link);
+
+fetch(api_link)
+{
+    
+}
