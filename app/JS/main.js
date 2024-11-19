3dfc180ae7d7adb4b237b5d6fd8e0ce28786e37e
johnnyw <jwu38438@gmail.com>
2024-11-18 21:34:27 -0500

    yay

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,9 +1,1 @@
-import "../css/style.css";
-const player_name =  'Junenaah'
-const api_link = `https://api.hypixel.net/player?key=${temporary_key}&name=${player_name}`;
-console.log(api_link);
-
-fetch(api_link)
-{
-    
-}
+import '../CSS/style.css';
