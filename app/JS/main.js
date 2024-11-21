82e083e45bff46be719f73673925afda0b0accf1
johnnyw <jwu38438@gmail.com>
2024-11-21 14:00:01 -0500

    wgwgwgwgfwegwegw

--- app/JS/main.js
+++ app/JS/main.js
@@ -4,6 +4,8 @@ const player_name = "Junenaah";
 const DOMSelectors = 
 {
     inputted_username:document.querySelector(".search_thingie"),
+    skyblock_levels:document.querySelector(".skyblock_levels"),
+    player_name:document.querySelector(".player_name")
 }
 const URL_SELECTORS=
 {
@@ -11,8 +13,10 @@ const URL_SELECTORS=
     auctions:"https://api.hypixel.net/skyblock/auctions",
     
 }
-const uuid =  (await getData(URL_SELECTORS.uuid)).uuid;
+const uuid =  ((await getData(URL_SELECTORS.uuid)).uuid).replace(/-/g, '');
+//Get uuid and replace hyphens with ""
 const example = `https://api.hypixel.net/player?uuid=${uuid}&key=${apiKey}`;
+//Test
 console.log(uuid)
 async function getData(url)
 {
@@ -26,12 +30,17 @@ async function getData(url)
         console.error("PROBLEM FETCHING DATA",error);
     }
 }
-const fetch_skyblock_data = await getData(`https://api.hypixel.net/v2/resources/skyblock/collections`);
+
 const test = await getData(`https://api.hypixel.net/v2/skyblock/profiles?key=${apiKey}&uuid=${uuid}`);
+//Fetch Player Profiles
 const current_profile =  test.profiles.find(profile=>{if(profile.selected){return profile;}})
-console.log(current_profile.profile_id);
+//Gives current Profile playeris on 
+console.log(current_profile.members[uuid]);
+
 const auction_current_profile= await getData(`https://api.hypixel.net/v2/skyblock/auction?key=${apiKey}&profile=${current_profile.profile_id}`)
-console.log(auction_current_profile);
+//Gets the auctions of a given player on thier current profile
+DOMSelectors.player_name.textContent=player_name;
 
 
-
+const fetch_skyblock_data = await getData(`https://api.hypixel.net/v2/resources/skyblock/collections`);
+//Collection Data
